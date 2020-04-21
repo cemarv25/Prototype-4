e07624fa07eed4948d75230f90bf6cadf50f0e50
Cesar Martinez <cesar.martinez0010@outlook.com>
2020-04-21 17:28:57 -0600

    Added PlayerController script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody playerRB;
+    public float speed = 5;
+    GameObject focalPoint;
+
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRB.AddForce(focalPoint.transform.forward * speed * forwardInput * Time.deltaTime);
+
+    }
+}
