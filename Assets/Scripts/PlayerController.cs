2754c44cea3dad14785e43d32e8495ace8a5f778
Cesar Martinez <cesar.martinez0010@outlook.com>
2020-04-24 17:53:29 -0600

    Added powerup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,15 @@ public class PlayerController : MonoBehaviour
     Rigidbody playerRB;
     public float speed = 5;
     GameObject focalPoint;
+    public GameObject powerUpIndicator;
+    public bool hasPowerUp;
+    public float powerUpStrength = 15.0f;
 
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("FocalPoint");
+        powerUpIndicator.SetActive(false);
     }
 
     // Update is called once per frame
@@ -19,6 +23,37 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRB.AddForce(focalPoint.transform.forward * speed * forwardInput * Time.deltaTime);
+        powerUpIndicator.transform.position = transform.position;
 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountdownRoutine());
+            powerUpIndicator.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Debug.Log($"El player colisiono con {collision.gameObject}" );
+            Rigidbody enemyRB = collision.gameObject.GetComponent<Rigidbody>();
+
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+            enemyRB.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+        }
+    }
+
+    IEnumerator PowerUpCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerUp = false;
+        powerUpIndicator.SetActive(false);
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    float spawnRange = 9;
 
 
     void Start()
     {
-        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
     }
 
+    Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+    }
 }
